20fe68d1986573bb7caf9262f22c385ee9c95562
Tamara Lehman <53117543+tamisil@users.noreply.github.com>
2023-08-14 13:51:46 -0600

    Update phase1_test.s

--- src/phase1_test.s
+++ src/phase1_test.s
@@ -12,12 +12,10 @@ _start:
 	.global _start
 
  addi	x4, x0, 'f'			// place your first initial, 'lower case', between the single quotes such as 'f'
-// slli	x4, x4, 8
  addi	x4, x4, 'l'			// place your last initial, 'lower case', between the single quotes such as 'l'
- //slli	x4, x4, 8
  addi	x3, x0, 0xa5		// load 0xa5 into x3
  nop					// use the register view and record the value in register x3
- or		x5, x4, x3
+ add		x5, x4, x3
  nop					// use the register view and record the value in register x5
  nop
  nop                    // at the end of this comment, provide the value in the register x3. x3 =
